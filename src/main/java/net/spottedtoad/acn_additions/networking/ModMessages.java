8c78a256f9c63e2def44ce4934ae71db9a9c5f43
SpottedToad <deepspacekind@gmail.com>
2026-06-16 02:33:25 -0600

    mod messages doesnt do anything right now

---
+++ src/main/java/net/spottedtoad/acn_additions/networking/ModMessages.java
@@ -0,0 +1,17 @@
+package net.spottedtoad.acn_additions.networking;
+
+import net.minecraft.util.Identifier;
+import net.spottedtoad.acn_additions.ACNMod;
+
+public class ModMessages {
+    public static final Identifier EXAMPLE_ID = new Identifier(ACNMod.MOD_ID, "example");;
+
+
+    public static void registerC2SPackets(){
+
+    }
+
+    public static void registerS2CPackets(){
+
+    }
+}
